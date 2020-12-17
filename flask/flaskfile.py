4e9e7432c528c3f84613d8cd9e084a337bdc6e39
dancorrea86 <danielmacielcorrea@gmail.com>
2020-12-16 22:26:53 -0300

    commit

--- flask/flaskfile.py
+++ flask/flaskfile.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    valor = int(request.form['projectFilePath']) + 1 
+    valor = str(valor)
+    return (valor)
 
 if __name__ == '__main__':
     app.run(debug=True)
