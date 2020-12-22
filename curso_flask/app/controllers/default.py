b1dc3b5d5ba3b0da972237ed3f64261cf8ad677c
dancorrea86 <danielmacielcorrea@gmail.com>
2020-12-22 19:05:07 -0300

    commit

--- curso_flask/app/controllers/default.py
+++ curso_flask/app/controllers/default.py
@@ -1,6 +1,22 @@
 from app import app
 
-
+@app.route("/index")
 @app.route("/")
 def index():
     return "Hellor world!"
+
+@app.route("/test", defaults={'name': None})
+@app.route("/test/<name>")
+def test(name):
+    if name:
+        return "Olá, %s" % name
+    else:
+        return "Olá, usuário"
+
+# @app.route("/test", defaults={'id': 0})
+# @app.route("/test/<int:id>")
+# def test(id):
+#     if id:
+#         return str(id)
+#     else:
+#         return "Não encontrado"
