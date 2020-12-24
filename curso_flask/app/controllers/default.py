de369969013e0ad7ff17b08e6f07245dc6dcdeae
dancorrea86 <danielmacielcorrea@gmail.com>
2020-12-24 16:56:42 -0300

    commit

--- curso_flask/app/controllers/default.py
+++ curso_flask/app/controllers/default.py
@@ -1,5 +1,7 @@
 from flask import render_template
-from app import app
+from app import app, db
+
+from app.models.tables import User
 from app.models.forms import LoginForm
 
 @app.route("/index")
@@ -17,3 +19,11 @@ def login():
         print(form.errors)
     return render_template('login.html',
                             form=form)
+
+@app.route("/teste/<info>")
+@app.route("/teste", defaults={"info": None})
+def teste(info):
+    i = User("testgdddgded", "1234d5d", "Julia Rizza", "testddes@gmail.com")
+    db. session.add(i)
+    db.session.commit()
+    return "Ok"
--- curso_flask/app/models/tables.py
+++ curso_flask/app/models/tables.py
@@ -1,11 +1,11 @@
 from app import db
 
-class User(db.model):
+class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Intenger, primay_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
-    password = db.Column(db.String, unique=True)
+    # password = db.Column(db.String)
     name = db.Column(db.String)
     email = db.Column(db.String(120), unique=True)
 
@@ -18,14 +18,14 @@ class User(db.model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-class User(db.model):
+class Post(db.Model):
     __tablename__ = "posts"
 
-    id = db.Column(db.Intenger, primay_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
-    user_id = db.Column(db.Intenger, db.ForeignKey=('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    user = db.relationship('User', foreign_key=user_id)
+    user = db.relationship('User', foreign_keys=user_id)
 
     def __init__(self, content, user_id):
         self.content = content
@@ -34,15 +34,15 @@ class User(db.model):
     def __repr__(self):
         return '<Post %r>' % self.id
 
-class User(db.model):
+class Follow(db.Model):
     __tablename__ = "follow"
 
-    id = db.Column(db.Intenger, primay_key=True)
-    user_id = db.Column(db.Intenger, db.ForeignKey=('users.id'))
-    follower_id = db.Column(db.Intenger, db.ForeignKey=('users.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    user = db.relationship('User', foreign_key=user_id)
-    follower = db.relationship('User', foreign_key=follower_id)
+    user = db.relationship('User', foreign_keys=user_id)
+    follower = db.relationship('User', foreign_keys=follower_id)
 
     def __init__(self, content, user_id):
         self.content = content
