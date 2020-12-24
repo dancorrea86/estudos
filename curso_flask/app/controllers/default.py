c940310dccbef6b6019176c069466ba859001c28
dancorrea86 <danielmacielcorrea@gmail.com>
2020-12-23 23:15:45 -0300

    first templete

--- curso_flask/app/controllers/default.py
+++ curso_flask/app/controllers/default.py
@@ -1,22 +1,8 @@
+from flask import render_template
 from app import app
 
-@app.route("/index")
-@app.route("/")
-def index():
-    return "Hellor world!"
-
-@app.route("/test", defaults={'name': None})
-@app.route("/test/<name>")
-def test(name):
-    if name:
-        return "Olá, %s" % name
-    else:
-        return "Olá, usuário"
-
-# @app.route("/test", defaults={'id': 0})
-# @app.route("/test/<int:id>")
-# def test(id):
-#     if id:
-#         return str(id)
-#     else:
-#         return "Não encontrado"
+@app.route("/index/<user>")
+@app.route("/", defaults={"user":None})
+def index(user):
+    return render_template('index.html',
+                            user=user)
