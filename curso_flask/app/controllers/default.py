bc7bdce7d1339bfe0138f035530334bfcc1ee7f5
dancorrea86 <danielmacielcorrea@gmail.com>
2020-12-25 11:28:55 -0300

    add read update delete

--- curso_flask/app/controllers/default.py
+++ curso_flask/app/controllers/default.py
@@ -20,9 +20,34 @@ def login():
     return render_template('login.html',
                             form=form)
 
+# @app.route("/teste/<info>")
+# @app.route("/teste", defaults={"info": None})
+# def teste(info):
+#     i = User("dancorre2", "1234d5d", "Daniel", "dan@gmail.com")
+#     db. session.add(i)
+#     db.session.commit()
+#     return "Ok"
+
+# @app.route("/teste/<info>")
+# @app.route("/teste", defaults={"info": None})
+# def teste(info):
+#     r = User.query.filter_by(password="1234d5d").first()
+#     print (r.username, r.name)
+#     return "Ok"
+
+# @app.route("/teste/<info>")
+# @app.route("/teste", defaults={"info": None})
+# def teste(info):
+#     r = User.query.filter_by(username="dancorrea86").first()
+#     r.username = "dancorrea"
+#     db. session.add(r)
+#     db.session.commit()
+#     return "Ok"
+
 @app.route("/teste/<info>")
 @app.route("/teste", defaults={"info": None})
 def teste(info):
-    r = User.query.filter_by(password="1234").all()
-    print (r)
+    r = User.query.filter_by(username="dancorrea").first()
+    db. session.delete(r)
+    db.session.commit()
     return "Ok"
