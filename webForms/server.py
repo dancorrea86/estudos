8639bebc468504fc2b99643b7b2ad14e3c53e25d
dancorrea86 <danielmacielcorrea@gmail.com>
2021-01-27 23:40:04 -0300

    commit

---
+++ webForms/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from forms import SignUpForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thecodex'
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
+@app.route('/about')
+def about():
+    posts = [{'title': 'Technology in 2019', 'author': 'Avi'},
+             {'title': 'Aprendendo Flask', 'author': 'Daniel'}]
+    return render_template('blog.html', author="Bob", sunny=True, posts=posts)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.is_submitted():
+        result = request.form
+        return render_template('user.html', result=result)
+    return render_template('signup.html', form=form)
+
+if __name__ == '__main__':
+    app.run()
